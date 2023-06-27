761679220a0bcf7e1daee36031eefffb4812288f
Mohamed Gasser Mohamed <102036714+mohamed-gasser@users.noreply.github.com>
2023-06-27 03:06:47 +0300

    Add files via upload

---
+++ FlightBuffer.java
@@ -0,0 +1,10 @@
+package full_project;
+
+public interface FlightBuffer {
+
+    public void queryReservation(String name) throws InterruptedException;
+    public void cancelReservation(String name, int seatNo, long customerId) throws InterruptedException;
+    public void cancelAll();
+    public void makeReservation(String name, int seatNo, long customerId) throws InterruptedException;
+    public int seatCount();
+}
